4111479202797b15d8f298f202efecaf6f44d2bc
Harini S <46838768+harinisridhar1310@users.noreply.github.com>
2019-02-01 20:39:59 +0530

    Update positive.py

--- positive.py
+++ positive.py
@@ -1,8 +1,8 @@
 #Harini
-a=raw_input()
-if a>0:
-	print("Positive")
-elif a<0:
-	print("Negative")
+a=input()
+if(a<0):
+	print"Negative"
+elif(a>0):
+	print"Positive"
 else:
-	print("Zero")
+	print"Zero"
