1e333b7ae7831010501bd56c7d431942b32e5a00
Harini S <46838768+harinisridhar1310@users.noreply.github.com>
2019-02-04 00:06:59 -0800

    Create primeint.py

---
+++ primeint.py
@@ -0,0 +1,10 @@
+#harini
+lower = int(input(" "))
+upper = int(input(""))
+for num in range(lower+1,upper + 1):
+	if num > 1:
+		for i in range(2,num):
+			if (num % i) == 0:
+				break
+		else:
+			print(num)
