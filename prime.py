b6c58316a6ebdfa565e8b23bf4efb13a9c60ec43
Harini S <46838768+harinisridhar1310@users.noreply.github.com>
2019-02-03 19:55:43 -0800

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,11 @@
+#harini
+num =input()
+if num > 1: 
+   for i in range(2, num//2): 
+       if (num % i) == 0: 
+           print("no") 
+           break
+   else: 
+       print("yes")
+else: 
+   print("no")
