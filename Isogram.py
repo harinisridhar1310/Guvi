5951f3616e8e05db473e53f7a7f1fe85bcc92a06
Harini S <46838768+harinisridhar1310@users.noreply.github.com>
2019-06-08 21:34:35 -0700

    Create Isogram.py

---
+++ Isogram.py
@@ -0,0 +1,11 @@
+#harini
+x=input()
+count=0
+for i in range(0,len(x)):
+    for j in range(i+1,len(x)):
+        if(x[i]==x[j]):
+            count=count+1
+if count==0:
+    print("Yes")
+else:
+    print("No")
