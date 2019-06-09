67713db7c1a0bd2f27f9294870817003a1c595ac
Harini S <46838768+harinisridhar1310@users.noreply.github.com>
2019-06-08 21:41:00 -0700

    Create GCD.py

---
+++ GCD.py
@@ -0,0 +1,16 @@
+#harini
+a,b=input().split()
+a=int(a)
+b=int(b)
+if a>b:
+    num=a
+    denom=b
+else:
+    num=b
+    denom=a
+rem=num%denom
+while(rem!=0):
+    num=denom
+    denom=rem
+    rem=num%denom
+print(denom)
