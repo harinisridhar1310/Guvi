4ba4bc0895bf3551b0005e635f12701f0f3cfb19
Harini S <46838768+harinisridhar1310@users.noreply.github.com>
2019-06-09 21:57:42 -0700

    Create Player_set3_21.py

---
+++ Player_set3_21.py
@@ -0,0 +1,14 @@
+#harini
+a=list(map(int,input().split()))
+x1=a[0]
+y1=a[1]
+b=list(map(int,input().split()))
+x2=a[0]
+y2=a[1]
+c=list(map(int,input().split()))
+x3=a[0]
+y3=a[1]
+if(x1==x2==x3 or y1==y2==y3):
+	print("yes")
+else:
+	print("no")
