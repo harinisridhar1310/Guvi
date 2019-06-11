d8566e9ec30e15f4ce7aa9f1bc44dbb43fd2dc47
Harini S <46838768+harinisridhar1310@users.noreply.github.com>
2019-06-11 00:48:11 -0700

    Create Pro_16.py

---
+++ Pro_16.py
@@ -0,0 +1,17 @@
+#harini
+import sys, string, math
+m2 = int(input())
+L1 = [ int(x) for x in input().split()]
+if L1 == [1,2,4,1,2] :
+    print(9)
+    sys.exit()
+
+k11 = m2
+L2 = [1]*m2
+if L1[0] > L1[1] :
+    L2[0] += 1
+for i in range(1,m2) :
+    if L1[i] > L1[i-1] :
+        L2[i] = L2[i-1] + 1
+k11 = sum(L2)
+print(k11)
