1ffeb77933c9e5d16968f097fefe7fba8d34d3c1
Harini S <46838768+harinisridhar1310@users.noreply.github.com>
2019-06-11 00:46:12 -0700

    Create Pro_15.py

---
+++ Pro_15.py
@@ -0,0 +1,17 @@
+#harini
+nt=int(input())
+m=list(map(int,input().split()))
+d=t=[]
+for i in range(0,nt):
+    d=list(bin(m[i]))
+    d=d[2:]
+    t.append(d)
+t=sorted(t)
+t=t[::-1]
+for i in range(0,nt):
+    k=1
+    x=0
+    for j in range(len(t[i])-1,-1,-1):
+        x=x+(int(t[i][j])*k)
+        k=k*2
+    print(x)
