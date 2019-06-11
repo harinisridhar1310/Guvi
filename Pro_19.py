1cd146d5efe756decf3bc526c557722fa69f32e2
Harini S <46838768+harinisridhar1310@users.noreply.github.com>
2019-06-11 00:55:10 -0700

    Create Pro_19.py

---
+++ Pro_19.py
@@ -0,0 +1,11 @@
+#harini
+n66=int(input())
+m2=[]
+b2=[]
+for i in range(0,n66):
+    m2=[int(x) for x in input().split()]
+    for j in range(0,len(m2)):
+        b2.append(m2[j])
+    m2=[]    
+b2.sort()
+print(*b2)
