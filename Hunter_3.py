f6703c548cc11e4df5f765b1534ff97c6541a59a
Harini S <46838768+harinisridhar1310@users.noreply.github.com>
2019-06-10 23:07:02 -0700

    Update Hunter_3.py

--- Hunter_3.py
+++ Hunter_3.py
@@ -1,15 +1,13 @@
 #harini
-a=int(input())
-b=list(map(int,input().split()))
-c=[]
-d=''
-for i in range(0,len(b)):
-	if(i==b[i]):
-		c.append(b[i])
-if(len(c)>0):
-	c.sort()
-	for j in range(0,len(c)):
-		d=d+str(c[j])+" "
-	print(d)
-else:
-	print(int(-1))
+n=int(input())
+m=list(map(int,input().split(" ")))
+l=[]
+for i in range(len(m)):
+    if(i==m[i]):
+        l.append(i)
+if l==[]:
+   print("-1")
+else:        
+  l.sort()
+  for i in l:
+    print(i,end=" ")  
